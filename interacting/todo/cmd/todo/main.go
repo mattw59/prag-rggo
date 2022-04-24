525183ff5be14d7087b1b467b982c778d2944fb2
Matthew White <matthew.white@crowe.com>
2022-04-24 14:13:10 -0400

    completed ch 2

--- interacting/todo/cmd/todo/main.go
+++ interacting/todo/cmd/todo/main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 
 	"pragprog.com/rggo/interacting/todo"
 )
 
-// Hardcoding the file name
-const todoFileName = ".todo.json"
+// default file name
+var todoFileName = ".todo.json"
 
 func main() {
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(),
+			"%s tool. Developed for The Pragmatic Bookshelf\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(),"Copyright 2020\n")
+		fmt.Fprintf(flag.CommandLine.Output(),"Usage information:\n")
+		flag.PrintDefaults()
+	}
+
+	// Parsing command line flags
+	add := flag.Bool("add",false,"Add task to the ToDo list")
+	list := flag.Bool("list",false,"List all tasks")
+	complete := flag.Int("complete",0,"Item to be completed")
+
+	flag.Parse()
+
+	if os.Getenv("TODO_FILENAME") != "" {
+		todoFileName = os.Getenv("TODO_FILENAME")
+	}
+
 	// Define an items list
 	l := &todo.List{}
 
@@ -21,27 +44,62 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Decide what to do base on the number of arguments provided
+	// Decide what to do based on the provided flags
 	switch {
-	//For no extra arguments, print the list
-	case len(os.Args) == 1:
-		// List the current to do items
-		for _, item := range *l {
-			fmt.Println(item.Task)
+	// List current to do items
+	case *list:
+		// List current to do items
+		fmt.Print(l)
+	case *complete > 0:
+		// Complete the given item
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr,err)
+			os.Exit(1)
 		}
-	// Concatenate all provided arguments with a space
-	// and add to the list as an item
-	default:
-		// Concatenate all arguments with a space
-		item := strings.Join(os.Args[1:], " ")
-
-		// Add the task
-		l.Add(item)
-
-		// Save the new list
-		if err := l.Save(todoFileName); err != nil {
+		//Save the new list
+		if err:= l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+	case *add:
+		// When any arguments (excluding flags) are provided, they will be
+		// used as the new task
+		t, err := getTask(os.Stdin, flag.Args()...)
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		l.Add(t)
+
+		//Save the new list
+		if err:= l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	default:
+		// Invalit flag provided
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
 	}
+
+}
+
+// getTask function decides where to get the description for a new 
+// task from : arguments or STDIN
+func getTask(r io.Reader, args ...string)(string, error) {
+	if len(args) >0 {
+		return strings.Join(args, " "), nil
+	}
+
+	s := bufio.NewScanner(r)
+	s.Scan()
+	if err := s.Err(); err != nil {
+		return "",err
+	}
+	
+	if len(s.Text()) == 0 {
+		return "",fmt.Errorf("Task cannot be blank")
+	}
+	return s.Text(),nil
 }
--- interacting/todo/cmd/todo/main_test.go
+++ interacting/todo/cmd/todo/main_test.go
@@ -2,15 +2,16 @@ package main_test
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
-	"strings"
 	"testing"
 )
+
 var (
-	binName = "todo"
+	binName  = "todo"
 	fileName = ".todo.json"
 )
 
@@ -21,10 +22,10 @@ func TestMain(m *testing.M) {
 		binName += ".exe"
 	}
 
-	build := exec.Command("go","build","-o",binName)
+	build := exec.Command("go", "build", "-o", binName)
 
 	if err := build.Run(); err != nil {
-		fmt.Fprintf(os.Stderr,"Cannot build tool %s: %s",binName,err)
+		fmt.Fprintf(os.Stderr, "Cannot build tool %s: %s", binName, err)
 		os.Exit(1)
 	}
 
@@ -50,7 +51,22 @@ func TestTodoCLI(t *testing.T) {
 	cmdPath := filepath.Join(dir, binName)
 
 	t.Run("AddNewTask", func(t *testing.T) {
-		cmd := exec.Command(cmdPath, strings.Split(task, " ")...)
+		cmd := exec.Command(cmdPath, "-add", task)
+
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	task2 := "test task number 2"
+	t.Run("AddNewTaskFromSTDIN", func(t *testing.T) {
+		cmd := exec.Command(cmdPath,"-add")
+		cmdStdIn, err := cmd.StdinPipe()
+		if err != nil {
+			t.Fatal(err)
+		}
+		io.WriteString(cmdStdIn, task2)
+		cmdStdIn.Close()
 
 		if err := cmd.Run(); err != nil {
 			t.Fatal(err)
@@ -58,18 +74,17 @@ func TestTodoCLI(t *testing.T) {
 	})
 
 	t.Run("ListTasks", func(t *testing.T) {
-		cmd := exec.Command(cmdPath)
+		cmd := exec.Command(cmdPath, "-list")
 		out, err := cmd.CombinedOutput()
 
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		expected := task + "\n"
+		expected := fmt.Sprintf(" 1: %s\n 2: %s\n", task, task2)
 
 		if expected != string(out) {
 			t.Errorf("Expected %q, got %q instead\n", expected, string(out))
 		}
 	})
 }
-
